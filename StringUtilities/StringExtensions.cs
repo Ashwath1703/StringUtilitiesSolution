4345dfd226bf6a5f9badd70e7f16665afd7e4186
Ashwath Sharma <ashar723@my.centennialcollege.ca>
2026-04-06 02:54:18 -0400

    Initial commit: string extension library

---
+++ StringUtilities/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace StringUtilities
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Returns true if the string starts with an upper-case letter.
+        /// </summary>
+        public static bool StartsWithUpperCase(this string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return false;
+
+            return char.IsUpper(value[0]);
+        }
+    }
+}
