bb1c8afc7d9e15d78a68b48c7e5a93c850579e01
Ashwath Sharma <ashar723@my.centennialcollege.ca>
2026-04-16 21:32:30 -0400

    Add console app consuming StringUtilitiesAshwath NuGet package

---
+++ StringUtilitiesApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using StringUtilities;
+
+string[] testStrings = { "Hello", "world", "Canada", "devops", "Ashwath" };
+
+foreach (var s in testStrings)
+{
+    Console.WriteLine($"\"{s}\" starts with uppercase: {s.StartsWithUpperCase()}");
+}
